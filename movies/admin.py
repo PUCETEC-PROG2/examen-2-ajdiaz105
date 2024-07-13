08bcb17930da647c3ad5c835150da4a8f5f6c580
Ajdiazs <ajdiazs@puce.edu.ec>
2024-07-13 12:43:08 -0500

    Final

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+@admin.register(Movie)
+class Movieadmin(admin.ModelAdmin):
+    pass
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title=models.CharField(max_length=100,null=False)
+    generous=models.CharField(max_length=100,null=False)
+    name = models.CharField(max_length=30,null=False)
+    year = models.IntegerField(null=False,default=1)
+    synopsis = models.TextField(max_length=330,null=False)
+
+    def __str__(self) -> str:
+        return self.name
